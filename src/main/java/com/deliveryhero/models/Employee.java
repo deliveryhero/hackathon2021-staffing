efdfe978bfbdd473644cf024bb58aac7a542961b
AminaZoheir <amina.zoheir@talabat.com>
2021-07-29 15:14:06 +0200

    Add list of unavailabilities to Employee model

--- src/main/java/com/deliveryhero/models/Employee.java
+++ src/main/java/com/deliveryhero/models/Employee.java
@@ -4,6 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -18,5 +22,37 @@ public class Employee {
     private int maxDurationPerDayHours;
     private int minDurationPerWeekHours;
     private int maxDurationPerWeekHours;
-    private int minBreakDurationHours;    
+    private int minBreakDurationHours;
+    private List<TimeSlot> unavailableTimes;
+
+    public Employee(String employeeId,
+            String startingPointId,
+            int maxShiftsPerDay,
+            int minShiftDurationHours,
+            int minShiftsPerDay,
+            int maxShiftDurationHours,
+            int minDurationPerDayHours,
+            int maxDurationPerDayHours,
+            int minDurationPerWeekHours,
+            int maxDurationPerWeekHours,
+            int minBreakDurationHours) {
+
+        this.employeeId = employeeId;
+        this.startingPointId = startingPointId;
+        this.maxShiftsPerDay = maxShiftsPerDay;
+        this.minBreakDurationHours = minShiftDurationHours;
+        this.minShiftsPerDay = minShiftsPerDay;
+        this.maxShiftDurationHours = maxShiftDurationHours;
+        this.minDurationPerDayHours = minDurationPerDayHours;
+        this.maxDurationPerDayHours = maxDurationPerDayHours;
+        this.minDurationPerWeekHours = minDurationPerWeekHours;
+        this.maxDurationPerWeekHours = maxDurationPerWeekHours;
+        this.minBreakDurationHours = minBreakDurationHours;
+        this.unavailableTimes = new ArrayList<>();
+
+    }
+
+    public List<TimeSlot> getUnavailableTimes() {
+        return unavailableTimes;
+    }
 }
---
+++ src/main/java/com/deliveryhero/models/TimeSlot.java
@@ -0,0 +1,15 @@
+package com.deliveryhero.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TimeSlot {
+    private Instant start;
+    private Instant end;
+}
