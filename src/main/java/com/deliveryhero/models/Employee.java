cea0ba635ecebd8719764416625e590d71952a6b
viv-dh <vivek.sukumaran.ext@deliveryhero.com>
2021-07-30 09:21:13 +0530

    fixed min shift duration bug

--- src/main/java/com/deliveryhero/models/Employee.java
+++ src/main/java/com/deliveryhero/models/Employee.java
@@ -1,12 +1,11 @@
 package com.deliveryhero.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -23,39 +22,11 @@ public class Employee {
     private int minDurationPerWeekHours;
     private int maxDurationPerWeekHours;
     private int minBreakDurationHours;
-    private List<TimeRange> unavailableTimes;
-    private List<TimeRange> shifts;
-    private List<TimeRange> shiftsAndBreaks;
+    private final List<TimeRange> unavailableTimes = new ArrayList<>();
+    private final List<TimeRange> shifts = new ArrayList<>();
+    private final List<TimeRange> shiftsAndBreaks = new ArrayList<>();
 
-    public Employee(String employeeId,
-            String startingPointId,
-            int maxShiftsPerDay,
-            int minShiftDurationHours,
-            int minShiftsPerDay,
-            int maxShiftDurationHours,
-            int minDurationPerDayHours,
-            int maxDurationPerDayHours,
-            int minDurationPerWeekHours,
-            int maxDurationPerWeekHours,
-            int minBreakDurationHours) {
-
-        this.employeeId = employeeId;
-        this.startingPointId = startingPointId;
-        this.maxShiftsPerDay = maxShiftsPerDay;
-        this.minBreakDurationHours = minShiftDurationHours;
-        this.minShiftsPerDay = minShiftsPerDay;
-        this.maxShiftDurationHours = maxShiftDurationHours;
-        this.minDurationPerDayHours = minDurationPerDayHours;
-        this.maxDurationPerDayHours = maxDurationPerDayHours;
-        this.minDurationPerWeekHours = minDurationPerWeekHours;
-        this.maxDurationPerWeekHours = maxDurationPerWeekHours;
-        this.minBreakDurationHours = minBreakDurationHours;
-        this.unavailableTimes = new ArrayList<>();
-        this.shifts = new ArrayList<>();
-        this.shiftsAndBreaks = new ArrayList<>();
-    }
-
-    public void addShift(TimeRange shift) {
+    public void addShift(final TimeRange shift) {
         shifts.add(shift);
         shiftsAndBreaks.add(new TimeRange(Instant.ofEpochSecond(shift.getStart().getEpochSecond() - getMinBreakDurationHours() * 3600),
                 Instant.ofEpochSecond(shift.getEnd().getEpochSecond() + getMinBreakDurationHours() * 3600)));
